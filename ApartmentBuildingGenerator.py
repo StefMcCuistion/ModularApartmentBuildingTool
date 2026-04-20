d6107648052cdc8c0a0736aa143dec582f73f1dc
Stef McCuistion <stef.mccuistion@gmail.com>
2026-04-20 04:51:54 -0500

    Added build and cancel buttons with connected signals. Added rooftop tiles to asset list.

--- ApartmentBuildingGenerator.py
+++ ApartmentBuildingGenerator.py
@@ -16,3 +16,24 @@ class Window(QtWidgets.QDialog):
         super().__init__(parent=get_maya_main_win())
         self.setWindowTitle("Apartment Building Generator")
         self.resize(500, 200)
+        self._mk_main_layout()
+
+    def _connect_signals(self):
+        self.cancel_btn.clicked.connect(self.close)
+        self.build_btn.clicked.connect(self.build)
+
+    def _mk_main_layout(self):
+        self.main_layout = QtWidgets.QVBoxLayout()  # vertical layout
+        # Make parameter UI
+        self.make_buttons_ui()  # Build and Cancel buttons
+        self.setLayout(self.main_layout)
+        self._connect_signals()
+
+    def make_buttons_ui(self):
+        self.build_btn = QtWidgets.QPushButton("Build")
+        self.cancel_btn = QtWidgets.QPushButton("Cancel")
+        self.main_layout.addWidget(self.build_btn)
+        self.main_layout.addWidget(self.cancel_btn)
+
+    def build(self):
+        print("Build")
