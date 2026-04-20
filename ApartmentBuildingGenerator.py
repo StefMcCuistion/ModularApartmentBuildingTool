806159cc23880b76e5d9de20bbc5e29c3ddca77d
Stef McCuistion <stef.mccuistion@gmail.com>
2026-04-20 04:41:37 -0500

    Python module can now display a Maya window

---
+++ ApartmentBuildingGenerator.py
@@ -0,0 +1,18 @@
+import maya.cmds as cmds
+import maya.OpenMayaUI as omui
+from PySide6 import QtWidgets, QtCore
+from shiboken6 import wrapInstance
+
+
+def get_maya_main_win():
+    """Return the Maya main window"""
+    main_win_addr = omui.MQtUtil.mainWindow()
+    return wrapInstance(int(main_win_addr), QtWidgets.QWidget)
+
+
+class Window(QtWidgets.QDialog):
+
+    def __init__(self):
+        super().__init__(parent=get_maya_main_win())
+        self.setWindowTitle("Apartment Building Generator")
+        self.resize(500, 200)
