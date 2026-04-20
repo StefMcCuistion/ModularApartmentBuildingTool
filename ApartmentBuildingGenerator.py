497c2639c36dce2d0d673bbfc915879e52a0c0a4
Stef McCuistion <stef.mccuistion@gmail.com>
2026-04-20 05:39:14 -0500

    Added balcony density slider plus smokestacks and sidewalk checkboxes

--- ApartmentBuildingGenerator.py
+++ ApartmentBuildingGenerator.py
@@ -79,12 +79,8 @@ class Window(QtWidgets.QDialog):
         self.details_lbl = QtWidgets.QLabel("Details")
         self.details_options_header_layout.addWidget(self.details_lbl)
         self.main_layout.addLayout(self.details_options_header_layout)
-        # balcony density (int 0 to 100 slider)
-        # Awning density (int 0 to 100 slider)
-        # Smokestacks (bool)
-        # Sidewalk (bool)
 
-        # door placement (int 0 to 100 slider, drives int 0 to width -1)
+        # door placement (drives int 0 to width -1)
         self.door_placement_layout = QtWidgets.QHBoxLayout()
         self.door_placement_lbl = QtWidgets.QLabel("Door Placement")
         self.door_placement_slider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
@@ -106,9 +102,34 @@ class Window(QtWidgets.QDialog):
         self.awning_density_layout.addWidget(self.awning_density_slider)
         self.main_layout.addLayout(self.awning_density_layout)
 
+        # balcony density
+        self.balcony_density_layout = QtWidgets.QHBoxLayout()
+        self.balcony_density_lbl = QtWidgets.QLabel("Balcony Density")
+        self.balcony_density_slider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
+        self.balcony_density_slider.setMinimum(0)
+        self.balcony_density_slider.setMaximum(100)
+        self.balcony_density_slider.setValue(80)
+        self.balcony_density_layout.addWidget(self.balcony_density_lbl)
+        self.balcony_density_layout.addWidget(self.balcony_density_slider)
+        self.main_layout.addLayout(self.balcony_density_layout)
+
         # smokestacks
+        self.smokestacks_layout = QtWidgets.QHBoxLayout()
+        self.smokestacks_lbl = QtWidgets.QLabel("Smokestacks")
+        self.smokestacks_checkbox = QtWidgets.QCheckBox()
+        self.smokestacks_checkbox.setChecked(True)
+        self.smokestacks_layout.addWidget(self.smokestacks_lbl)
+        self.smokestacks_layout.addWidget(self.smokestacks_checkbox)
+        self.main_layout.addLayout(self.smokestacks_layout)
 
         # sidewalk
+        self.sidewalk_layout = QtWidgets.QHBoxLayout()
+        self.sidewalk_lbl = QtWidgets.QLabel("Sidewalk")
+        self.sidewalk_checkbox = QtWidgets.QCheckBox()
+        self.sidewalk_checkbox.setChecked(True)
+        self.sidewalk_layout.addWidget(self.sidewalk_lbl)
+        self.sidewalk_layout.addWidget(self.sidewalk_checkbox)
+        self.main_layout.addLayout(self.sidewalk_layout)
 
     def make_buttons_ui(self):
         self.build_btn = QtWidgets.QPushButton("Build")
